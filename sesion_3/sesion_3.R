bb87b625c483bc4f84ef62eee00e318556a0cfc6
Mario Jaque <mario.jaque@mayor.cl>
2024-08-14 08:57:15 -0400

    Update sesion_3.R

--- sesion_3/sesion_3.R
+++ sesion_3/sesion_3.R
@@ -1,6 +1,5 @@
-#install.packages('pacman')
-
-# Importar librerías 
+#descargar pack
+install.packages("pacman")
 pacman::p_load(
   readxl, #importar archivos excel
   tidyverse, #manejo de datos
@@ -11,33 +10,30 @@ pacman::p_load(
   summarytools,
   rio
 )
-# Definir la ruta del archivo
+#definir la ruta del archivo
 ruta <- here('sesion_3',"data","data_le_anonima.xlsx")
-# Importar el archivo
-data <- import(ruta) 
-# Revisar columnas
-names(data)
-# Revisar los datos
-dfSummary(data)
 
-# Mismo formato de las columnas
-data <- data %>%
+#impotar el archivo
+dato <- import(ruta) 
+
+#revisar columnas 
+names(dato)
+#revisar los datos que tiene el archivo
+dfSummary(dato)
+
+dato <- dato %>% 
   clean_names()
 
-# importar base deis
-ruta <- here('sesion_3',"data","deis2024.xlsx")
-
-deis <- import(ruta) %>%
-  clean_names() %>%
-  select(codigo_vigente, nombre_oficial)  %>% 
+#importar nueva ruta para realizar cruce
+ruta2 <- here('sesion_3',"data","deis2024.xlsx")
+deis <- import (ruta2) %>% 
+clean_names() %>% 
+  select(codigo_vigente, nombre_oficial) %>% 
   mutate(
-    codigo_vigente = as.character(codigo_vigente)
+    codigo_vigente = as.character (codigo_vigente)
   )
 
-# cruzar los datos de LE y DEIS 
-data <- data %>%
-  left_join(deis, by=c('estab_orig'='codigo_vigente'))
-  
-
-
+#CRUZAR DATOS LE Y DEIS
+dato <- dato %>% 
+  left_join(deis, by= c ("estab_orig" = "codigo_vigente"))
 
